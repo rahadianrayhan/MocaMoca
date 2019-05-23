be7095a0945a6d3e6e78fe6da5a2220df642d293
WINDOWS-73E4E21\RAYHAN <rahadian.rayhan@gmail.com>
2019-05-24 00:52:34 +0700

    +lobby

---
+++ Mocha Combat/Assets/Scripts/Lobby.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Lobby : MonoBehaviourPunCallbacks {
+
+    public GameObject startButton;
+
+    private void Start()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.ConnectUsingSettings();
+        startButton.SetActive(true);
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Server");
+    }
+
+    public void OnClickStart()
+    {
+        PhotonNetwork.JoinRandomRoom();
+        StartCoroutine(Timeout());
+    }
+
+    IEnumerator Timeout()
+    {
+        yield return new WaitForSeconds(3f);
+        RoomOptions room = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = 3 };
+        int roomId = Random.Range(0, 1000);
+        PhotonNetwork.CreateRoom("Room " + roomId.ToString(), room);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        StopAllCoroutines();
+        Debug.Log("On Room : " + PhotonNetwork.CurrentRoom);
+        PhotonNetwork.LoadLevel("Room");
+    }
+
+}
---
+++ Mocha Combat/Assets/Scripts/RoomController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class RoomController : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(PhotonNetwork.CurrentRoom.PlayerCount);
+    }
+}
